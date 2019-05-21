027dcb78f735cc1a2ba01454f149d1a865d7a370
Zach Parker <parkerz410@gmail.com>
2019-05-21 13:15:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(string)
   string = "Hello there, and how are you?"
-  totalrev = string.reverse
-  #stringarray = string.split(" ")
-  #stringarray.each do |word|
-    #word.reverse
-  #end
+  stringarray = string.split(" ")
+  stringarray.each do |word|
+    word.reverse
+  end
+  stringarray.join
 end
